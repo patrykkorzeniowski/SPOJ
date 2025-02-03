2ccf2be8df54a967a63b6968c06bac8e3468dab7
Patryk Korzeniowski <patryk.korzeniowski3@gmail.com>
2025-02-03 23:40:23 +0100

    suma

---
+++ Suma/suma.py
@@ -0,0 +1,9 @@
+import sys
+
+def main():
+    suma = 0
+    for line in sys.stdin:  # Pobieranie danych aż do EOF
+        suma += int(liczba)  # Konwersja na liczbę i dodanie do sumy
+        print(suma)  # Wypisanie aktualnej sumy
+
+main()
