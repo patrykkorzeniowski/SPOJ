6a8af988adc96566a216cb46e53e16d0f536d111
Patryk Korzeniowski <patryk.korzeniowski3@gmail.com>
2025-02-03 22:23:04 +0100

    first commit

---
+++ Life, the Universe, and Everything/C++/Life, the Universe, and Everything.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    while (cin >> a) {
+    if (a == 42) {
+        break;
+    }
+        cout << a << endl;
+    }
+    
+    return 0;
+}
---
+++ Life, the Universe, and Everything/Python/Life, the Universe, and Everything.py
@@ -0,0 +1,8 @@
+def main():
+     while True:
+        num = int(input())
+        if num == 42:
+            break
+        else:
+            print(num)
+main()
