7332088dbd442dac16913f90247b88f2ab30d1e5
Patryk Korzeniowski <patryk.korzeniowski3@gmail.com>
2025-02-07 00:07:42 +0100

    suma c++

--- Suma/suma.py
+++ Suma/suma.py
@@ -3,7 +3,7 @@ import sys
 def main():
     suma = 0
     for line in sys.stdin:  # Pobieranie danych aż do EOF
-        suma += int(liczba)  # Konwersja na liczbę i dodanie do sumy
+        suma += int(line.strip())  # Konwersja na liczbę i dodanie do sumy
         print(suma)  # Wypisanie aktualnej sumy
 
 main()
